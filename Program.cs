188b3406afcf2607698016ae012599079d081221
VallNC <rvall2077@gmail.com>
2024-05-31 08:01:20 +0300

    Some fixing

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ Console.OutputEncoding = Encoding.UTF8;
 
 using HttpClient client = new HttpClient();
 {
-
-
+List<string> areaNameList = new List<string>();
+await NewMethod(client, areaNameList);
   client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
   client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
   client.DefaultRequestHeaders.Add("Referer", "https://example.com");
@@ -29,6 +29,7 @@ using HttpClient client = new HttpClient();
   try
     {
         
+
         await GrabEveryJson(client, areaList);
         int nameNumber = 0;
         foreach (var item in areaList)
@@ -74,16 +75,65 @@ using HttpClient client = new HttpClient();
   }
 }
 
+static async Task NewMethod(HttpClient client, List<string> areaNameList)
+{
+   var htmlWeb = new HtmlWeb();
+    var httpText = htmlWeb.Load("""https://www.cik.bg/bg/epns2024/candidates/ns?rik=0&party=0""");
+          string[] resultNames = httpText.Text.Split("\n\r".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+    int testNumber = 0;
+    for (int i = 0; i < resultNames.Length; i++)
+    {      
+     // System.Console.WriteLine(resultNames[i]);
+        if (resultNames[i].Contains("""<select class="rik" style="width: 100%">"""))
+        {
+            {
+              System.Console.WriteLine(resultNames[i]);
+                i++;
+                bool startFound = false;
+                for (int int2 = 0; int2 < resultNames[i].Length-1; int2++)
+                {
+                  if (resultNames[i][int2]==('\"'))
+                  {
+                      if (!startFound)
+                      {
+                          startFound=true;
+                      }
+                      else
+                      {
+                        int charEnd = 0;
+                        for (int int3 = int2; int3 < resultNames[i].Length; int3++,charEnd++)
+                        {
+                          if(resultNames[i][int3]==('<'))
+                          {
+                            break;
+                          }
+                        }
+                          areaNameList.Add(resultNames[i].Substring(int2,charEnd));
+                          startFound = false;
+                      }
+                      
+                      //System.Console.WriteLine(areaNameList[testNumber]);
+                      testNumber++;
+                      i++;
+                  }
+                }
+                break;
+            }
+        }
+
+    }
+}
+
 static async Task GrabEveryJson(HttpClient client, List<string> areaList)
 {
   //i<32
     for (int i = 1; i < 32; i++)
     {
-        if (i == 2)
+       // if (i == 2)
             //2ри район не връща json
-            continue;
+            //continue;
         string link = @"https://www.cik.bg/bg/epns2024/candidates/ns?rik=" + i + @"&party=0";
-        System.Console.WriteLine(link);
+        //System.Console.WriteLine(link);
         HttpResponseMessage response = await client.GetAsync(link);
         response.EnsureSuccessStatusCode();
 
@@ -129,6 +179,10 @@ public class Candidate
 
 
 }
+
+
+
+
 
 /*
 
